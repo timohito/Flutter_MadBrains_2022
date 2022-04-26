9cac54a201384baf36316fb76dfcd76ea3c1fc06
timohito <71021976+timohito@users.noreply.github.com>
2022-04-27 00:23:43 +0400

    1

--- lesson_1/lib/main.dart
+++ lesson_1/lib/main.dart
@@ -1,9 +1,66 @@
-import 'film.dart';
+import 'package:flutter/material.dart';
 
-import 'film_logic.dart';
+void main() => runApp(const MyApp());
 
-Future<void> main() async {
-  //FilmLogic filmLogic = FilmLogic();
-  //List<Film> list = await filmLogic.getList(randomList.length);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+// Основа нашего приложения
+    return MaterialApp(
+// Отображается над приложениями в Android, когда пользователь нажимает кнопку «последние приложения».
+      title: 'Films',
+// Стили всего приложения
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+// Наша начальная страница
+      home: const MyHomePage(title: 'Films'),
+    );
+  }
 }
 
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({required this.title, Key? key}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: _FilmList(),
+    );
+  }
+}
+
+class _FilmList extends StatelessWidget {
+  const _FilmList({Key? key}) : super(key: key);
+
+  static final List<String> films = <String>[
+    'Брат',
+    'Служебный роман',
+    'Волк с Уолл-стрит',
+    'Бриллиантовая рука',
+    'Интерстеллар',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: List.generate(
+        films.length,
+        (int index) => ColoredBox(
+          color: Colors.purple,
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text(films[index]),
+          ),
+        ),
+      ),
+    );
+  }
+}
