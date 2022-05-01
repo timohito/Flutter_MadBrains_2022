f0d5fe994ad70e9fd741a04d194bf31fb85e266e
timohito <71021976+timohito@users.noreply.github.com>
2022-05-02 02:37:31 +0400

    init

--- lesson_1/lib/main.dart
+++ lesson_1/lib/main.dart
@@ -7,60 +7,85 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-// Основа нашего приложения
+    // Основа нашего приложения
     return MaterialApp(
-// Отображается над приложениями в Android, когда пользователь нажимает кнопку «последние приложения».
+      debugShowCheckedModeBanner: false,
+      // Отображается над приложениями в Android, когда пользователь нажимает кнопку «последние приложения».
       title: 'Films',
-// Стили всего приложения
+      // Стили всего приложения
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        appBarTheme: const AppBarTheme(
+          color: Colors.cyan,
+        ),
       ),
-// Наша начальная страница
-      home: const MyHomePage(title: 'Films'),
+      // Наша начальная страница
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({required this.title, Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  final String title;
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: const Text('Список фильмов'),
       ),
-      body: _FilmList(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
 
-class _FilmList extends StatelessWidget {
-  const _FilmList({Key? key}) : super(key: key);
+class FilmCard extends StatelessWidget {
+  const FilmCard({
+    Key? key,
+    required this.name,
+    required this.picture,
+    required this.language,
+  }) : super(key: key);
 
-  static final List<String> films = <String>[
-    'Брат',
-    'Служебный роман',
-    'Волк с Уолл-стрит',
-    'Бриллиантовая рука',
-    'Интерстеллар',
-  ];
+  final String name;
+  final String picture;
+  final String language;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: List.generate(
-        films.length,
-        (int index) => ColoredBox(
-          color: Colors.purple,
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Text(films[index]),
-          ),
-        ),
-      ),
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      color: Colors.green,
     );
   }
 }
