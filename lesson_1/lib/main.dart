6a3d66213ae1606faaadbd093e580b44d29fbda2
timohito <71021976+timohito@users.noreply.github.com>
2022-05-07 03:55:49 +0400

    1:10, 51 slide

--- lesson_1/lib/main.dart
+++ lesson_1/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'app/models/film_card_model.dart';
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -55,48 +57,171 @@ class MyHomePage extends StatelessWidget {
 class _FilmList extends StatelessWidget {
   const _FilmList({Key? key}) : super(key: key);
 
-  static final List<String> films = <String>[
-    'Брат',
-    'Служебный роман',
-    'Волк с Уолл-стрит',
-    'Бриллиантовая рука',
-    'Интерстеллар',
+  static const List<FilmCardModel> _films = <FilmCardModel>[
+    FilmCardModel(
+      id: 0,
+      title: 'Брат',
+      voteAverage: 8.3,
+      picture:
+          'https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/e9008e2f-433f-43b0-b9b8-2ea8e3fb6c9b/600x900',
+      releaseDate: '1997',
+      description:
+          'Дембель Данила Багров защищает слабых в Петербурге 1990-х. Фильм, сделавший Сергея Бодрова народным героем.',
+    ),
+    FilmCardModel(
+      id: 1,
+      title: 'Служебный роман',
+      voteAverage: 8.3,
+      picture:
+          'https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/fd4e75bb-a6fe-46ef-86cd-0f470334fcbd/600x900',
+      releaseDate: '1977',
+      description:
+          'Робкий холостяк решает приударить за строгой начальницей. Комедия Эльдара Рязанова, классика советского кино.',
+    ),
+    FilmCardModel(
+      id: 2,
+      title: 'Волк с Уолл-стрит',
+      voteAverage: 7.9,
+      picture:
+          'https://avatars.mds.yandex.net/get-kinopoisk-image/1600647/c5876e81-9dec-43e2-923f-fee2fca85e21/576x',
+      releaseDate: '2013',
+      description:
+          'Восхождение циника-гедониста на бизнес-олимп 1980-х. Блистательный тандем Леонардо ДиКаприо и Мартина Скорсезе',
+    ),
+    FilmCardModel(
+      id: 3,
+      title: 'Бриллиантовая рука',
+      voteAverage: 8.5,
+      picture:
+          'https://avatars.mds.yandex.net/get-kinopoisk-image/1600647/a419d20d-4ae6-4c7c-91b3-c38ef9ca1ffe/600x900',
+      releaseDate: '1968',
+      description:
+          'Контрабандисты гоняются за примерным семьянином. Народная комедия с элементами абсурда от Леонида Гайдая',
+    ),
+    FilmCardModel(
+      id: 4,
+      title: 'Интерстеллар',
+      voteAverage: 8.6,
+      picture:
+          'https://avatars.mds.yandex.net/get-kinopoisk-image/1600647/430042eb-ee69-4818-aed0-a312400a26bf/600x900',
+      releaseDate: '2014',
+      description:
+          'Фантастический эпос про задыхающуюся Землю, космические полеты и парадоксы времени. «Оскар» за спецэффекты',
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: List.generate(
-        films.length,
-        (int index) => Padding(
-          padding: EdgeInsets.only(top: 20),
-          child: FilmTile(
-            title: films[index],
-          ),
-        ),
-      ),
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        return FilmTile.fromModel(model: _films[index % _films.length]);
+      },
     );
   }
 }
 
 class FilmTile extends StatelessWidget {
-  const FilmTile({Key? key, required this.title}) : super(key: key);
+  const FilmTile({
+    required this.id,
+    required this.title,
+    required this.picture,
+    required this.voteAverage,
+    required this.releaseDate,
+    required this.description,
+    Key? key,
+  }) : super(key: key);
+
+  factory FilmTile.fromModel({
+    required FilmCardModel model,
+    Key? key,
+  }) {
+    return FilmTile(
+      id: model.id,
+      title: model.title,
+      picture: model.picture,
+      voteAverage: model.voteAverage,
+      releaseDate: model.releaseDate,
+      description: model.description,
+      key: key,
+    );
+  }
+
+  final int id;
   final String title;
+  final String picture;
+  final double voteAverage;
+  final String releaseDate;
+  final String description;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
-        const FlutterLogo(),
-        Column(
-          children: <Widget>[
-            Text(title),
-            Row(
-              children: const <Widget>[Icon(Icons.star), Text('4,5')],
-            )
-          ],
-        )
+        Expanded(
+          child: Image.network(
+            picture,
+            loadingBuilder: (BuildContext context, Widget child,
+                ImageChunkEvent? loadingProgress) {
+              if (loadingProgress == null) {
+                return child;
+              }
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  title,
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: Row(
+                    children: <Widget>[
+                      const Padding(
+                        padding: EdgeInsets.only(right: 8),
+                        child: Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          voteAverage.toStringAsFixed(1),
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: voteAverage < 4
+                                ? Colors.red
+                                : voteAverage >= 8
+                                    ? Colors.green
+                                    : Colors.black,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16, bottom: 8),
+                  child: Text(
+                    'Дата выхода: $releaseDate',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                ),
+                Text(description),
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
